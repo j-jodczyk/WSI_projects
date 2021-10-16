a20cf4e22768aeb2dbdf73ffa7f9cf4201e1b26f
Julia Maria Jodczyk <01158711@pw.edu.pl>
2021-10-16 18:39:38 +0200

    fixed bugs in gradientDescend function, added plotting

--- zadanie1/zadanie1.py
+++ zadanie1/zadanie1.py
@@ -5,17 +5,31 @@
 #       z adaptacją parametru kroku przy użyciu metody z nawrotami.
 #           w iteracyjny sposób znaleźć
 
+import numpy as np
+import numdifftools as ndt
+from matplotlib import pyplot as plt
 
-# x to chyba powinien byc wektor, bo funkcja wieloargumentowa
-def gradientDescend(x0, grad, step, error_margin=0.0001):
+
+# chyba działa
+def gradientDescend(x0, func, step, alpha, n, error_margin=0.0001):
     # krok stały, podany przez użytkownika
     # x nalezy do D = [-100, 100]^n
     x = x0
     diff = x0
-    while diff>error_margin:
+    # for making sure that gradient works properly
+    num_of_iterations = 0
+    y = []
+
+    while any(i>error_margin for i in diff) and num_of_iterations<1000:
         prev_x = x
-        x = prev_x - step*grad(prev_x)
-        diff = abs(prev_x-x)
+        grad = [i*step for i in ndt.Gradient(func)(prev_x, alpha, n)]
+        x = [prev_x[i] - grad[i] for i in range(len(x))]
+        diff = [abs(prev_x[i]-x[i]) for i in range(len(x))]
+
+        num_of_iterations += 1
+        y.append(func(x, alpha, n))
+
+    return (x, y, num_of_iterations)
 
 
 # x wektor
@@ -27,6 +41,7 @@ def newtonConstStep(x0, grad, inv_hess, step, error_margin=0.0001):
         prev_x = x
         x = prev_x - step*inv_hess(prev_x)*grad(x)
         diff = abs(prev_x-x)
+    return x
 
 
 # x wektor
@@ -42,8 +57,20 @@ def newtonAdaptStep(x0, func, grad, inv_hess, step, error_margin=0.0001):
         diff = abs(prev_x-x)
 
 
+def f(x, alpha, n):
+    result = 0
+    for i in range(n):
+        result += alpha**(i/(n-1))*x[i]**2
+    return result
+
+
 def main():
-    pass
+    alpha = [1, 10, 100]
+    n = [10, 20]
+    x0 = [15]*10
+    min, y, num_of_iter = gradientDescend(x0, f, 0.2, alpha[0], n[0])
+    plt.plot(range(num_of_iter), y)
+    plt.show()
 
 
 if __name__=="__main__":
