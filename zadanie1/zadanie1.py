6702e5650325ee38d1f976de5a083d140cfe1ce0
Julia Maria Jodczyk <01158711@pw.edu.pl>
2021-10-20 21:04:38 +0200

    figured of that the step was too big

--- zadanie1/zadanie1.py
+++ zadanie1/zadanie1.py
@@ -101,7 +101,9 @@ def localMinimum(algorithm, x0, func, step, alpha, n, betha=0.75, gamma=0.25, er
         while (func(x+t*v, alpha, n)>func(x, alpha, n)+gamma*t*np.transpose(ndt.Gradient(func)(x, alpha, n))*v).any(): # tu any czy all???
             t = betha*t
 
-    while any(i>error_margin for i in diff) and num_of_iter<max_num_of_iter:
+
+    while np.linalg.norm(diff)>error_margin and num_of_iter<max_num_of_iter:
+        a = np.linalg.norm(diff)
         prev_x = x
 
         if algorithm=='gradientDescend' :
@@ -110,8 +112,16 @@ def localMinimum(algorithm, x0, func, step, alpha, n, betha=0.75, gamma=0.25, er
             d = np.asarray(np.dot(prev_hess, ndt.Gradient(func)(prev_x, alpha, n))).reshape(-1)
             #d = np.dot(np.linalg.inv(ndt.Hessian(f)(prev_x, alpha, n)), ndt.Gradient(func)(prev_x, alpha, n))
 
-        x = [prev_x[i] - t*step*d[i] for i in range(len(x))]
-        diff = [abs(prev_x[i]-x[i]) for i in range(len(x))]
+        x = prev_x - t*step*d
+        b = np.linalg.norm(d)
+
+        # zadana dziedzina x- czy jest na to potrzeba?
+        # for x_i in x:
+        #     if x_i>100:
+        #         x_i = 100
+        #     elif x_i<-100:
+        #         x_i = -100
+        diff = prev_x-x
 
         num_of_iter += 1
         next_y.append(func(x, alpha, n))
@@ -142,20 +152,20 @@ def inv_hess(prev_inv_hess, x_diff, grad_diff):
 
 def iterToStep(algorithm, fig, alpha, step, n, x0):
     min_step = []
-    if n == 10:
-        k = 0
-    else:
-        k = 3
+    k = 0 if n==10 else 3
     for j in range(len(alpha)):
         iterations = []
+        min_y = []
         for i in range(1, len(step-2)):
             f_min, next_x, next_y, num_of_iter = localMinimum(algorithm, x0, f, step[i], alpha[j], n)
             iterations.append(num_of_iter)
+            min_y.append(next_y[-1])
         ax = fig.add_subplot(2, 3, j+k+1)
-        plt.plot(step[1:], iterations)
+        # plt.plot(step[1:], iterations) # for which step we iterate the least
+        plt.plot(step[1:], min_y) # for which step do we get the smallest f
         min_id = iterations.index(min(iterations))
         plt.plot(step[min_id], iterations[min_id], 'y*')
-        min_step.append((step[min_id], iterations[min_id]))
+        min_step.append((step[min_id], iterations[min_id], f_min))
         ax.tick_params(labelsize=7)
         ax.title.set_text(f"alpha={alpha[j]}, n={n}")
         ax.title.set_size(7)
@@ -164,12 +174,22 @@ def iterToStep(algorithm, fig, alpha, step, n, x0):
     return min_step
 
 
+def iterToMin(algorithm, fig, alpha, step, n, x0):
+    k = 0 if n==10 else 3
+    color = ['g', 'b', 'r']
+    for j in range(len(alpha)):
+        ax = fig.add_subplot(2, 3, j+k+1)
+        for i in range(len(step[j])):
+            f_min, next_x, next_y, num_of_iter = localMinimum(algorithm, x0, f, step[j][i], alpha[j], n)
+            plt.plot(range(num_of_iter+1), next_y, f'{color[i]}')
+
+
 
 def main():
 
     alpha = [1, 10, 100]
     n = [10, 20]
-    step = np.linspace(0, 2, 30)
+    step = np.linspace(0, 1, 1000) #potrzebe nowe wykresy
     x0 = np.array([randint(1, 100) for i in range(10)])
     x1 = np.array([randint(1, 100) for i in range(20)])
 
@@ -201,16 +221,19 @@ def main():
     # min_step += iterToStep('gradientDescend', fig, alpha, step, 20, x1)
     # print(min_step)
 
-    fig2 = plt.figure()
-    min_step = iterToStep('newtonConstStep', fig2, alpha, step, 10, x0)
-    min_step += iterToStep('newtonConstStep', fig2, alpha, step, 20, x1)
-    print(min_step)
+    # fig2 = plt.figure()
+    # min_step = iterToStep('newtonConstStep', fig2, alpha, step, 10, x0)
+    # min_step += iterToStep('newtonConstStep', fig2, alpha, step, 20, x1)
+    # print(min_step)
 
     # fig3 = plt.figure()
     # min_step = iterToStep('newtonAdaptStep', fig3, alpha, step, 10, x0)
     # min_step += iterToStep('newtonAdaptStep', fig3, alpha, step, 20, x1)
     # print(min_step)
 
+    fig4 = plt.figure()
+    iterToMin('gradientDescend', fig4, alpha, [[0.25, 0.4, 0.9], [0.03, 0.02, 0.07], [0.01, 0.001, 0.0001]], 10, x0)
+    iterToMin('gradientDescend', fig4, alpha, [[0.25, 0.4, 0.9], [0.03, 0.02, 0.07], [0.01, 0.001, 0.0001]], 20, x1)
     plt.show()
     #plt.savefig("iteration_to_step")
 
