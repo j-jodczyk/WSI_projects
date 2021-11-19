0e7e7a57ebc9e29468a0ce76632ed5057ad57dab
Julia Maria Jodczyk <01158711@pw.edu.pl>
2021-11-19 18:24:41 +0100

    new task

---
+++ zadanie3/algorithm.py
@@ -0,0 +1,20 @@
+# wylacznie implementacja - nie trzeba dokumentacji
+# rozbicie na funkcje
+# Zaimplementuj algorytm MIN-MAX w grze kółko i krzyżyk
+# na planszy 3x3. Program powinien grać sam ze sobą i
+# wizualizować kolejne stany gry na terminalu.
+# Pamiętaj, że implementacja musi być wykonana samodzielnie.
+# Brak zrozumienia dostarczonego kodu rozwiązania równowazne jest plagiatowi.
+
+class State():
+    pass
+
+class Move():
+    pass
+
+class Player():
+    # polityka grania
+    pass
+
+class Game():
+    pass
