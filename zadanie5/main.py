7e028f9a2aaca8cf95d7118694b1e8713f18ccc7
Julia Maria Jodczyk <01158711@pw.edu.pl>
2022-01-16 23:04:26 +0100

    ready to test

--- zadanie5/main.py
+++ zadanie5/main.py
@@ -4,40 +4,41 @@
 # dyskonto i learning rate + wpływ strategii eksploracji (Bolzmanowska i zachlanna)
 # czas dochodzenia do nagrody - max 4 str
 
-from re import L
+
+from matplotlib import pyplot as plt
 import numpy as np
 import gym
 
 class Agent():
-    def __init__(self, actions: int, states: int, learning_rate: float, discount_rate: float) -> None:
+    def __init__(self, actions: int, states: int, learning_rate: float, discount_rate: float, policy: str) -> None:
         self.Q_table = np.zeros((states, actions)) #holds maximum expected future reward for each action at each state
         self.learning_rate = learning_rate
         self.discount_rate = discount_rate
-        # Q_table[action][state] = E(G|action, state)
+        self.policy = policy
 
-        # Q learning algorith
-        #   choose action
-        #   perform action
-        #   measure revard
-        #   update Q
+    def choose_action(self, state, param, env):
+        if self.policy == "greedy":
+            exploration = np.random.random(1)[0]
+            if exploration > param:
+                action = np.argmax(self.Q_table[state, :])
+            else:
+                action = env.action_space.sample()
+        else:
+            exp_sum = np.sum([np.exp(self.Q_table[state, a]/param) for a in range(len(self.Q_table[state]))])
+            action_probs = [(np.exp(self.Q_table[state, a]/param))/exp_sum for a in range(len(self.Q_table[state]))]
+            action = np.random.choice(list(range(len(self.Q_table[state, :]))), p=action_probs)
+        return action
 
-        # exploration_rate - rate of steps we'll do randomly, start with = 1
-        # we generate random number, if number > exploration_rate -> exploatation (what we already know)
-        #   else - explore
 
     def update_table(self, state, action, reward, next_step):
         # update:
-        # Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s’,a’) — Q(s,a)]
+        # Q(s,a):= Q(s,a) + lr [R(s,a) + dr * max Q(s’,a’) — Q(s,a)]
         self.Q_table[state, action] = self.Q_table[state, action] + self.learning_rate *\
                                     (reward + self.discount_rate * np.max(self.Q_table[next_step, :] \
                                     - self.Q_table[state, action]))
 
-    def choose_action(self) -> None:
-        pass
-
 def main():
     env = gym.make('Taxi-v3')
-    env.render()
 
     actions = env.action_space.n
     states = env.observation_space.n
@@ -46,75 +47,66 @@ def main():
     train_episodes = 50000
     test_episodes = 100
     steps = 100
-    learning_rate = 0.7
+    learning_rate = 0.3
     discount_rate = 0.6             # the larger the dr, the more agent cares about long term reward
+    exploration_rate = 0.3
+    temperature = 500
+    policy = "greedy"
 
-    exploration_rate = 1            # 1 is max
-    min_er = 0.01                   # min exploration rate
-    exploration_decay_rate = 0.01
 
-    agent = Agent(actions, states, learning_rate, discount_rate)
+    avg_steps = 0
+    avg_reward = 0
+    for i in range(20):
 
-    # cumulative expected reward : G = sum(gamma**k * reward_in_state[k])
-    # learning methods
-    # monte carlo - collect revards at the end of episode and calulate maximum expected future reward
-    #   max(expected future reward at state) = former estimation + learning_rate*(G - former estimation)
-    # temporal difference - esimate reward at each step
-    #   max(expected future reward at state) = former estimation + learinig_rate*(previous reward + gamma*discounted value on the next step - previous estimate)
+        agent = Agent(actions, states, learning_rate, discount_rate, policy)
 
-    # approaches to RL
-    # value based - goal to optimize value function
-    # policy based - optimize the policy function without using value function
-    #   deterministic policy
-    #   stochastic policy - output a distribution probability over actions
-    # model based - model the environment
+        for episode in range(train_episodes):
+            state = env.reset()
+            done = False
 
-    for episode in range(train_episodes):
-        state = env.reset()
-        done = False
+            for step in range(steps):
+                # decision if we explore or exploit
+                param = exploration_rate if agent.policy == "greedy" else temperature
+                action = agent.choose_action(state, param, env)
+                # execute action and see what happens
+                next_step, reward, done, info = env.step(action)
 
-        for step in range(steps):
-            # decision if we explore or exploit
-            exploration = np.random.random(1)[0]
-            if exploration > exploration_rate:
+                # update Q_table
+                agent.update_table(state, action, reward, next_step)
+
+                state = next_step
+
+                if done:
+                    break
+
+        # tests:
+        rewards = []
+        for episode in range(test_episodes):
+            state = env.reset()
+            done = False
+
+            steps_eval = 0
+            reward_eval = 0
+
+            for step in range(steps):
                 action = np.argmax(agent.Q_table[state, :])
-            else:
-                action = env.action_space.sample()
+                next_step, reward, done, info = env.step(action)
 
-            # execute action and see what happens
-            next_step, reward, done, info = env.step(action)
+                reward_eval += reward
 
-            # update Q_table
-            agent.update_table(state, action, reward, next_step)
+                if done:
+                    rewards.append(reward_eval)
+                    steps_eval += step
+                    break
 
-            state = next_step
+                state = next_step
+        env.close()
+        avg_steps += steps_eval
+        avg_reward += sum(rewards)/test_episodes
 
-            if done:
-                break
+    print(f"Avg steps: {avg_steps/20}")
+    print(f"Avg reward: {avg_reward/20}")
 
-        # decay exploration_rate
-        exploration_rate = min_er + (1 - min_er) * np.exp(-exploration_decay_rate*episode)
-
-    all_rewards = []
-
-    for episode in range(test_episodes):
-        state = env.reset()
-        done = False
-        rewards = 0
-
-        for step in range(steps):
-            action = np.argmax(agent.Q_table[state, :])
-            next_step, reward, done, info = env.step(action)
-
-            rewards += reward
-
-            if done:
-                all_rewards.append(rewards)
-                break
-
-            state = next_step
-    env.close()
-    print(f"score: {sum(all_rewards)/test_episodes}")
 
 if __name__=="__main__":
     main()
